5c9c956f6b93e7760cdd193fca9ac89a5ab0c702
swap22 <sharma.sswapnil@gmail.com>
2017-07-28 15:20:02 +0530

    connect db with welcome

--- pages/welcome.php
+++ pages/welcome.php
@@ -1,2 +1,20 @@
-<h1>Welcome to Task Manager!</h1>
+<h1>Welcome to myToDo!</h1>
+ 
+<?php
+if($_SESSION['logged_in']){
+//Instantiate Database object
+
+
+
+
+ 
+$database = new Database;
+ 
+//Get logged in user
+$list_user = $_SESSION['username'];
+ 
+//Query
+$database->query('SELECT * FROM lists WHERE list_user=:list_user');
+$database->bind(':list_user',$list_user);
+$rows = $database->resultset();
  
