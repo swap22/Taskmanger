1e5f228d3e40425da0f1524f012292247fd6bad9
swap22 <sharma.sswapnil@gmail.com>
2017-07-29 21:32:50 +0530

    Task button

--- pages/task.php
+++ pages/task.php
@@ -1,2 +1,14 @@
+<?php
+     $task_id = $_GET['id'];
+//Instantiate Database object
+$database = new Database;
+//Query
+$database->query('SELECT * FROM tasks WHERE id = :id');
+$database->bind(':id',$task_id);
+$row = $database->single();
+ 
 
+?>
+<br />
+<br />
 <a href="?page=edit_task&id=<?php echo $row['id']; ?>">Edit Task</a>
