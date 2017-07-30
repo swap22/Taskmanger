028936ec1810abaf4c0fb846a5ea7f8de99c2a45
swap22 <sharma.sswapnil@gmail.com>
2017-07-30 10:35:41 +0530

    connect db with register

--- pages/register.php
+++ pages/register.php
@@ -1,4 +1,86 @@
-
+<?php if($_POST['register_submit']){
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$email = $_POST['email'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$password2 = $_POST['password2'];
+		$errors = array();
+ 
+		//Check passwords match
+		if($password != $password2){
+			$errors[] = "Your passwords do not match";
+		} 
+		//Check first name
+		if(empty($first_name)){
+			$errors[] = "First Name is Required";
+		} 
+		//Check email
+		if(empty($email)){
+			$errors[] = "Email is Required";
+		} 
+		//Check username
+		if(empty($username)){
+			$errors[] = "Username is Required";
+		} 
+		//Match passwords
+		if(empty($password)){
+			$errors[] = "Password is Required";
+		} 
+ 
+ 
+		//Instantiate Database object
+		$database = new Database;
+ 
+		/* Check to see if username has been used */
+ 
+		//Query
+		$database->query('SELECT username FROM users WHERE username = :username');
+		$database->bind(':username', $username);  
+		//Execute
+		$database->execute();
+		if($database->rowCount() > 0){
+			$errors[] = "Sorry, that username is taken";
+		}
+ 
+		/* Check to see if email has been used */
+ 
+		//Query
+		$database->query('SELECT email FROM users WHERE email = :email');
+		$database->bind(':email', $email);  
+		//Execute
+		$database->execute();
+		if($database->rowCount() > 0){
+			$errors[] = "Sorry, that email is taken";
+		}
+ 
+		//If there are no errors, proceed with registration
+		if(empty($errors)){
+			//Encrypt Password
+			$enc_password = md5($password);
+ 
+			//Query
+			$database->query('INSERT INTO users (first_name,last_name,email,username,password)
+			              VALUES(:first_name,:last_name,:email,:username,:password)');
+			//Bind Values
+			$database->bind(':first_name', $first_name);  
+			$database->bind(':last_name', $last_name);   
+			$database->bind(':email', $email);  
+			$database->bind(':username', $username);  
+			$database->bind(':password', $enc_password);  
+ 
+			//Execute
+			$database->execute();
+ 
+			//If row was inserted
+			if($database->lastInsertId()){
+				echo '<p class="msg">You are now registered! Please Log In</p>';
+			} else {
+				echo '<p class="error">Sorry, something went wrong. Contact the site admin</p>';
+			}
+		}
+}
+?>
  
 <h3>Register</h3>
 <p>Please use the form below to register at our site</p>
