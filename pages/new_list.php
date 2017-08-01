207c40843fe61822f436af8f997c7ab78f692f2f
swap22 <sharma.sswapnil@gmail.com>
2017-08-01 19:55:47 +0530

    modified list display

--- pages/new_list.php
+++ pages/new_list.php
@@ -5,9 +5,21 @@
 		$list_body = $_POST['list_body'];
 		$list_user = $_SESSION['username'];
  
-		
-?>
+		//Instantiate Database object
+		$database = new Database;
  
+		$database->query('INSERT INTO lists (list_name,list_body,list_user) VALUES(:list_name,:list_body,:list_user)');
+		$database->bind(':list_name',$list_name);
+		$database->bind(':list_body',$list_body);
+		$database->bind(':list_user',$list_user);
+		$database->execute();
+ 
+		if($database->lastInsertId()){
+ 
+			echo '<p class="msg">Your list has been created</p>';
+		}
+	}
+?>
  
  
 <h1>Add a List</h1>
