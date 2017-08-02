353e205494ceb3f60909ff000494dd4daaec8ff7
swap22 <sharma.sswapnil@gmail.com>
2017-08-02 17:07:34 +0530

    db connect

--- pages/new_task.php
+++ pages/new_task.php
@@ -1,4 +1,26 @@
-
+<?php
+	if($_POST['task_submit']){
+		$task_name = $_POST['task_name'];
+		$task_body = $_POST['task_body'];
+		$due_date = $_POST['due_date'];
+		$list_id = $_POST['list_id'];
+ 
+		//Instantiate Database object
+		$database = new Database;
+ 
+		$database->query('INSERT INTO tasks (task_name,task_body,due_date,list_id) VALUES(:task_name,:task_body,:due_date,:list_id)');
+		$database->bind(':task_name',$task_name);
+		$database->bind(':task_body',$task_body);
+		$database->bind(':due_date',$due_date);
+		$database->bind(':list_id',$list_id);
+		$database->execute();
+		if($database->lastInsertId()){
+			echo '<p class="msg">Your task has been created</p>';
+		}
+ 
+	}
+ 
+?>
  
 <h1>Add a Task</h1>
 <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
