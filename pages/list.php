3c4bb3d80b763d59f16e2958ff8fea1880fe8750
swap22 <sharma.sswapnil@gmail.com>
2017-08-05 18:20:11 +0530

    final list display

--- pages/list.php
+++ pages/list.php
@@ -1,6 +1,26 @@
 <?php
 $list_id = $_GET['id'];
-  
+ 
+//Instantiate Database object
+$database = new Database;
+//Query
+$database->query('SELECT * FROM lists WHERE id = :id');
+$database->bind(':id',$list_id);
+$row = $database->single();
+ 
+echo '<h1>'.$row['list_name'].'</h1>';
+echo '<p>'.$row['list_body'].'</p>';
+echo '<a href="?page=edit_list&id='.$row['id'].'">Edit List</a> | ';
+echo '<a href="?page=delete_list&id='.$row['id'].'">Delete List</a>';
+ 
+//Instantiate Database object
+$database = new Database;
+//Query
+$database->query('SELECT * FROM tasks WHERE list_id = :list_id AND is_complete = :is_complete');
+$database->bind(':list_id',$list_id);
+$database->bind(':is_complete',0);
+$rows = $database->resultset();
+ 
 echo '<h3>Tasks</h3>';
 if($rows){
 echo '<ul class="items">';
